65c71e09c46a6286d3f5197f07986d0d4b49ec6e
Raynagah <rodrigo.vargas.rogel99@gmail.com>
2025-11-06 16:02:29 -0300

    test5

--- src/components/PersonaForm.js
+++ src/components/PersonaForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import React, { useState } from 'react';
 import axios from "axios";
 
 const API_URL : string = "https://backend-fs-d-k547.onrender.com/api/personas";
--- src/components/PersonaList.js
+++ src/components/PersonaList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-
+import React, { useState, useEffect } from 'react';
 const API_URL : string = "https://backend-fs-d-k547.onrender.com/api/personas";
 
 function PersonaList(){

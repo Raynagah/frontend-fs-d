cc61007c8c05ead41fabbae06844eba20ad832da
Raynagah <rodrigo.vargas.rogel99@gmail.com>
2025-11-06 15:38:39 -0300

    test

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import PersonaForm from './components/PersonaForm';
+import PersonaList from './components/PersonaList';
+
+function App() {
+  return (
+    <div className = "container mt-5">
+        <h1 className = "text-center mb-4">CRUD de persona </h1>
+        <PersonaForm />
+        <PersonaList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PersonaList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+
+const API_URL : string = "https://backend-fs-d-swtn.onrender.com/api/personas";
+
+function PersonaList(){
+    const [personas, setPersonas] = useState([]);
+
+    const cargar = async () =>{
+        const res = await axios.get(API_URL);
+        setPersonas(res.data)
+    };
+
+    const eliminar = async (id) =>{
+        await axios.delete(`${API_URL}/${id}`);
+        cargar()
+    };
+
+    useEffect(() =>{
+    cargar();
+    } ,[]);
+
+    return(
+    <ul className = "list-group">
+        {personas.map(p =>(
+            <li key = "{p.id}"
+                className = "list-group-item d-flex justify-content-between">
+                    {p.nombre}
+                    <button className = "btn btn-danger btn-sm" onClick={()=>eliminar(p.id)}>Eliminar</button>
+                </li>
+        ))}
+    </ul>
+    );
+}
+
+export default PersonaList;
